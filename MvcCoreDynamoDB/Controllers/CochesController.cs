5cb0a2741c6278f9beda943481aafc7ef79ab55d
Enrique Javier Munoz Romera <EnriqueJavier.Munoz@tajamar365.com>
2021-05-07 13:38:05 +0200

    commit

---
+++ MvcCoreDynamoDB/Controllers/CochesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDynamoDB.Models;
+using MvcCoreDynamoDB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDynamoDB.Controllers
+{
+    public class CochesController : Controller
+    {
+        ServiceDynamoDB Service;
+
+        public CochesController(ServiceDynamoDB service) { this.Service = service; }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await this.Service.GetCoches());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await this.Service.FindCoche(id));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction("Index","Coches");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Coche car)
+        {
+            return RedirectToAction("Index", "Coches");
+        }
+    }
+}
---
+++ MvcCoreDynamoDB/Services/ServiceDynamoDB.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using MvcCoreDynamoDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDynamoDB.Services
+{
+    public class ServiceDynamoDB
+    {
+        private DynamoDBContext context;
+
+        public void ServiceAWSDynamoDb()
+        {
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            this.context = new DynamoDBContext(client);
+        }
+
+        public async Task CreateCoche(Coche car)
+        {
+            await this.context.SaveAsync<Coche>(car);
+        }
+
+        public async Task<List<Coche>> GetCoches()
+        {
+            var tabla = this.context.GetTargetTable<Coche>();
+            var scanOptions = new ScanOperationConfig();
+            //scanOptions.PaginationToken = "";
+            var results = tabla.Scan(scanOptions);
+            //Los datos que recuperamos son document y devuelve una coleccion
+            List<Document> data = await results.GetNextSetAsync();
+            //Debemos transformar dichos datos a su tippado
+            //esto es automatico mediante un metodo
+            IEnumerable<Coche> cars = this.context.FromDocuments<Coche>(data);
+            return cars.ToList();
+        }
+
+        public async Task<Coche> FindCoche(int idcoche)
+        {
+            //si etamos buscando por patiition key(PK) HASKEY solamente debemos hacerlo con load
+            //esto es equivalente a buscar con consulta
+            return await this.context.LoadAsync<Coche>(idcoche);
+        }
+
+        public async Task DeleteCoche(int idCoche)
+        {
+            await this.context.DeleteAsync<Coche>(idCoche);
+        }
+    }
+}
