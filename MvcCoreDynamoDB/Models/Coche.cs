ece24f10653f6f0551ae50b1a906cb56b99c3822
Enrique Javier Munoz Romera <EnriqueJavier.Munoz@tajamar365.com>
2021-05-07 11:51:34 +0200

    [model OK]

---
+++ MvcCoreDynamoDB/Models/Coche.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDynamoDB.Models
+{
+    [DynamoDBTable("coches")]
+    public class Coche
+    {
+        [DynamoDBHashKey]
+        [DynamoDBProperty("idcoche")]
+        public int IdCoche { get; set; }
+
+        [DynamoDBProperty("marca")]
+        public String Marca { get; set; }
+
+        [DynamoDBProperty("modelo")]
+        public String Modelo { get; set; }
+
+        [DynamoDBProperty("vmax")]
+        public int VMax { get; set; }
+
+    }
+}
